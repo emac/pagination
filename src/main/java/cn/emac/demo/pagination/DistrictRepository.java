ae8b75a5544f2c8141751fd4eeb37904855a7483
Emac <shen.bin.1983@gmail.com>
2016-04-10 23:00:43 +0800

    use @Repository

--- src/main/java/cn/emac/demo/pagination/DistrictRepository.java
+++ src/main/java/cn/emac/demo/pagination/DistrictRepository.java
@@ -1,14 +1,12 @@
 package cn.emac.demo.pagination;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author Emac
  * @since 2016-04-10
  */
-public interface DistrictRepository extends Repository<District, Long> {
-
-    Page<District> findAll(Pageable pageable);
+@Repository
+public interface DistrictRepository extends PagingAndSortingRepository<District, Long> {
 }
